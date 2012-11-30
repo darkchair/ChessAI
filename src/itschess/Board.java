779365e16aa02ad6cf138297ca41631d0358e3a7
Ian Wills <urafatrodent@gmail.com>
2012-11-29 20:57:32 -0500

    incorporated and structured code

---
+++ src/itschess/Board.java
@@ -0,0 +1,454 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itschess;
+
+/**
+ *
+ * @author Ian
+ */
+public class Board
+{
+    public byte board[][] = new byte [8][8];
+    public byte attackboard[][] = new byte[8][8];
+
+    public Board (){
+        byte[] holder = {-3,-5,-4,-2,-1,-4,-5,-3};
+        board[0] = holder; 
+        for(int i = 0; i < 8; i ++)
+        {
+                board[1][i] = -6;
+
+        }
+        for(int i = 2; i < 6; i ++)
+        {
+                for(int j = 0; j < 8; j ++)
+                {
+                        board[i][j] = 0;
+                }
+        }
+        for(int i = 0; i < 8; i ++)
+        {
+                board[6][i] = 6;
+
+        }
+        board[7][0] = 3;
+        board[7][1] = 5;
+        board[7][2] = 4;
+        board[7][3] = 2;
+        board[7][4] = 1;
+        board[7][5] = 4;
+        board[7][6] = 5;
+        board[7][7] = 3;
+
+        for(byte i = 0; i < 8; i ++)
+        {
+                for(byte j = 0; j <8; j++)
+                {
+                        attackboard[i][j] = 0;
+                }
+        }
+    }
+
+    public void analysis(){
+        byte[][] moves;
+        for(byte i = 0; i < 8; i ++)
+        {
+            for(byte j = 0; j <8; j++)
+            {
+                if(board[i][j] == (byte) -6)
+                {
+                    moves = possibleMovesPB(j,i);
+                    for(byte k = 0; k < moves.length; k ++)
+                    {
+                            if(moves[k][0] != (byte)100)
+                            {
+                                    //movePiece(i,j,moves[k][0], moves[k][1]);
+                                    attackboard[moves[k][0]][moves[k][1]] = (byte) (attackboard[moves[k][0]][moves[k][1]] + 1);
+                            }
+                    }
+                }
+                else if (board[i][j] == (byte) -5)
+                {
+                    moves = possibleMovesN(j,i);
+                    for(byte k = 0; k < moves.length; k ++)
+                    {
+                            if(moves[k][0] != (byte)100)
+                            {
+                                    //movePiece(i,j,moves[k][0], moves[k][1]);
+                                    attackboard[moves[k][0]][moves[k][1]] = (byte) (attackboard[moves[k][0]][moves[k][1]] + 1);
+                            }
+                    }
+                }
+                else if (board[i][j] == (byte) -4)
+                {
+                    moves = possibleMovesBishop(j,i);
+                    for(byte k = 0; k < moves.length; k ++)
+                    {
+                            if(moves[k][0] != (byte)100)
+                            {
+                                    //movePiece(i,j,moves[k][0], moves[k][1]);
+                                    attackboard[moves[k][0]][moves[k][1]] = (byte) (attackboard[moves[k][0]][moves[k][1]] + 1);
+                            }
+                    }
+                }
+            }
+        }
+    }
+
+    public void movePiece(byte x, byte y, byte x1, byte y1)
+    {
+            board[x1][y1] = board[x][y];
+    }
+
+    public void move()
+    {
+
+    }
+
+    public byte[][] possibleMovesPB(byte x, byte y){
+            byte[][] retVal = new byte[4][2];
+            if(board[y+1][(byte) (x)] == 0)
+            {
+                    retVal[0][0] = (byte) (y+1);
+                    retVal[0][1] = x;
+            }
+            else
+            {
+                    retVal[0][0] = (byte) 100;
+                    retVal[0][1] = (byte) 100;
+            }
+            if(board[(byte) (y+2)][x] == 0)
+            {
+                    retVal[1][0] = (byte) (y+2);
+                    retVal[1][1] = x;
+            }
+            else
+            {
+                    retVal[0][0] = (byte) 100;
+                    retVal[0][1] = (byte) 100;
+            }
+            if(x+1 < 8 && board[(byte) (y+1)][(byte) (x+1)] == 0 )
+            {
+                    retVal[2][0] = (byte) (y+1);
+                    retVal[2][1] = (byte) (x+1);
+            }
+            else
+            {
+                    retVal[2][0] = (byte) 100;
+                    retVal[2][1] = (byte) 100;
+            }
+            if(x > 0 && board[(byte) (y+1)][(byte) (x-1)] == 0 )
+            {
+                    retVal[3][0] = (byte) (y+1);
+                    retVal[3][1] = (byte) (x-1);
+            }
+            else
+            {
+                    retVal[3][0] = (byte)100;
+                    retVal[3][1] = (byte)100;
+            }
+            return retVal;
+    }
+
+    public byte[][] possibleMovesP(byte x, byte y){
+        byte[][] retVal = new byte[4][2];
+        if(board[x][(byte) (y+1)] == 0  && y<8)
+        {
+                retVal[0][0] = x;
+                retVal[0][1] = (byte) (y+1);
+        }
+        else
+        {
+                retVal[0][0] = (byte) 100;
+                retVal[0][1] = (byte) 100;
+        }
+        if(board[x][(byte) (y+2)] == 0)
+        {
+                retVal[1][0] = x;
+                retVal[1][1] = (byte) (y+2);
+        }
+        else
+        {
+                retVal[0][0] = (byte) 100;
+                retVal[0][1] = (byte) 100;
+        }
+        if(board[(byte) (x+1)][(byte) (y+1)] == 0)
+        {
+                retVal[2][0] = (byte) (x+1);
+                retVal[2][1] = (byte) (y+1);
+        }
+        else
+        {
+                retVal[2][0] = (byte) 100;
+                retVal[2][1] = (byte) 100;
+        }
+        if(board[(byte) (x-1)][(byte) (y+1)] == 0)
+        {
+                retVal[3][0] = (byte) (x-1);
+                retVal[3][1] = (byte) (y+1);
+        }
+        else
+        {
+                retVal[3][0] = (byte)100;
+                retVal[3][1] = (byte)100;
+        }
+        return retVal;
+    }
+
+    public byte[][] possibleMovesN(byte x, byte y){
+        byte[][] retVal = new byte[8][2];
+        if(x>0 && y<6 && board[(byte) x-1][(byte) y+2] == 0)
+        {
+                retVal[0][0] = (byte) (x-1);
+                retVal[0][1] = (byte) (y+2);
+        }
+        else
+        {
+                retVal[0][0] = (byte) 100;
+                retVal[0][1] = (byte) 100;
+        }
+        if(x<7 && y<6 && board[ (byte) x+1][(byte) y+2] == 0)
+        {
+                retVal[1][0] = (byte) (x+1);
+                retVal[1][1] = (byte) (y+2);
+        }
+        else
+        {
+                retVal[1][0] = (byte) 100;
+                retVal[1][1] = (byte) 100;
+        }
+        if(x<6 && y<7 &&board[(byte) (x+(byte)2)][(byte) (y+(byte)1)] == 0)
+        {
+                retVal[2][0] = (byte) (x+2);
+                retVal[2][1] = (byte) (y+1);
+        }
+        else
+        {
+                retVal[2][0] = (byte) 100;
+                retVal[2][1] = (byte) 100;
+        }
+        if(x>1 && y<7 && board[(byte) (x-2)][(byte) (y+1)] == 0)
+        {
+                retVal[3][0] = (byte) (x-(byte)2);
+                retVal[3][1] = (byte) (y+(byte)1);
+        }
+        else
+        {
+                retVal[3][0] = (byte)100;
+                retVal[3][1] = (byte)100;
+        }
+        if(x<6 && y>0 && board[(byte) (x+2)][(byte) (y-1)] == 0)
+        {
+                retVal[2][0] = (byte) (x+(byte)2);
+                retVal[2][1] = (byte) (y-(byte)1);
+        }
+        else
+        {
+                retVal[2][0] = (byte) 100;
+                retVal[2][1] = (byte) 100;
+        }
+        if(x>1 && y>0 && board[(byte) (x-2)][(byte) (y-1)] == 0)
+        {
+                retVal[3][0] = (byte) (x-(byte)2);
+                retVal[3][1] = (byte) (y-(byte)1);
+        }
+        else
+        {
+                retVal[3][0] = (byte)100;
+                retVal[3][1] = (byte)100;
+        }
+        if(x<7 && y>1 && board[(byte) (x+(byte)1)][(byte) (y-(byte)2)] == 0)
+        {
+                retVal[2][0] = (byte) (x+(byte)1);
+                retVal[2][1] = (byte) (y-(byte)2);
+        }
+        else
+        {
+                retVal[2][0] = (byte) 100;
+                retVal[2][1] = (byte) 100;
+        }
+        if(x>0 && y>1 && board[(byte) (x-(byte)1)][(byte) (y-(byte)2)] == 0)
+        {
+                retVal[3][0] = (byte) (x-(byte)1);
+                retVal[3][1] = (byte) (y-(byte)2);
+        }
+        else
+        {
+                retVal[3][0] = (byte)100;
+                retVal[3][1] = (byte)100;
+        }
+        return retVal;
+    }
+
+    public byte[][] possibleMovesBishop(byte x, byte y)
+    {//returns a byte array of the possible moves a bishop can make
+        byte[][] retval = new byte[13][2];
+        byte sizecount = 0;
+
+        for(byte i=x, m=y; i<8 && m<8; i++,m++)
+        {
+            if(board[(i)][(m)] == 0)
+            {
+                retval[sizecount][0] = (byte)(i);
+                retval[sizecount][1] = (byte)(m);
+                sizecount++;
+            }
+            else
+            {
+                if(i != 7 && m != 7)
+                {
+                    retval[sizecount][0] = (byte)(i+1);
+                    retval[sizecount][1] = (byte)(m+1);
+                    sizecount++;
+                }
+                break;
+            }
+        }
+        for(byte i=x, m=y; i>0 && m<8; i--, m++)
+        {
+            if(board[(i)][(m)] == 0)
+            {
+                retval[sizecount][0] = (byte)(i);
+                retval[sizecount][1] = (byte)(m);
+                sizecount++;
+            }
+            else
+            {
+                if(i != 1 && m != 7)
+                {
+                    retval[sizecount][0] = (byte)(i-1);
+                    retval[sizecount][1] = (byte)(m+1);
+                    sizecount++;
+                }
+                break;
+            }
+        }
+        for(byte i=x, m=y; i<8 && m>0; i++, m--)
+        {
+            if(board[(i)][(m)] == 0)
+            {
+                retval[sizecount][0] = (byte)(i);
+                retval[sizecount][1] = (byte)(m);
+                sizecount++;
+            }
+            else
+            {
+                if(i != 7 && m != 1)
+                {
+                    retval[sizecount][0] = (byte)(i+1);
+                    retval[sizecount][1] = (byte)(m-1);
+                    sizecount++;
+                }
+                break;
+            }
+        }
+        for(byte i=x, m=y; i>0 && m>0; i--, m--)
+        {
+            if(board[(i)][(m)] == 0)
+            {
+                retval[sizecount][0] = (byte)(i);
+                retval[sizecount][1] = (byte)(m);
+                sizecount++;
+            }
+            else
+            {
+                if(i != 1 && m != 1)
+                {
+                    retval[sizecount][0] = (byte)(i-1);
+                    retval[sizecount][1] = (byte)(m-1);
+                    sizecount++;
+                }
+                break;
+            }
+        }
+
+        return retval;
+    }
+
+    public byte[][] possibleMovesRook(byte x, byte y)
+    {//returns a byte array of the possible moves a rook can make
+        byte[][] retval = new byte[14][2];
+        byte sizecount = 0;
+
+        for(byte i=x, m=y; i<8; i++)
+        {
+            if(board[(i)][(m)] == 0)
+            {
+                retval[sizecount][0] = (byte)(i);
+                retval[sizecount][1] = (byte)(m);
+                sizecount++;
+            }
+            else
+            {
+                if(i != 7)
+                {
+                    retval[sizecount][0] = (byte)(i+1);
+                    retval[sizecount][1] = (byte)(m);
+                    sizecount++;
+                }
+                break;
+            }
+        }
+        for(byte i=x, m=y; i>0; i--)
+        {
+            if(board[(i)][(m)] == 0)
+            {
+                retval[sizecount][0] = (byte)(i);
+                retval[sizecount][1] = (byte)(m);
+                sizecount++;
+            }
+            else
+            {
+                if(i != 1)
+                {
+                    retval[sizecount][0] = (byte)(i-1);
+                    retval[sizecount][1] = (byte)(m);
+                    sizecount++;
+                }
+                break;
+            }
+        }
+        for(byte i=x, m=y; m<8; m++)
+        {
+            if(board[(i)][(m)] == 0)
+            {
+                retval[sizecount][0] = (byte)(i);
+                retval[sizecount][1] = (byte)(m);
+                sizecount++;
+            }
+            else
+            {
+                if(m != 7)
+                {
+                    retval[sizecount][0] = (byte)(i);
+                    retval[sizecount][1] = (byte)(m+1);
+                    sizecount++;
+                }
+                break;
+            }
+        }
+        for(byte i=x, m=y; m>0; m--)
+        {
+            if(board[(i)][(m)] == 0)
+            {
+                retval[sizecount][0] = (byte)(i);
+                retval[sizecount][1] = (byte)(m);
+                sizecount++;
+            }
+            else
+            {
+                if(m != 1)
+                {
+                    retval[sizecount][0] = (byte)(i);
+                    retval[sizecount][1] = (byte)(m-1);
+                    sizecount++;
+                }
+                break;
+            }
+        }
+
+        return retval;
+    }
+}
--- src/itschess/ItsChess.java
+++ src/itschess/ItsChess.java
@@ -16,24 +16,14 @@ public class ItsChess {
     static byte depth = 1;
     static byte[][] possibleMoves;
     
-    public class Board
-    {
-        public byte[][] theBoard;
-        
-        public void move()
-        {
-            
-        }
-    }
-    
-    public void alphaBetaSearch(ItsChess.Board board)
+    public void alphaBetaSearch(Board board)
     {
         Integer a = new Integer(-50000); Integer b = new Integer(50000);
         int v = maxValue(board, a, b);
         
     }
     
-    public byte maxValue(ItsChess.Board board, Integer a, Integer b)
+    public byte maxValue(Board board, Integer a, Integer b)
     {
         byte v = -100;//initial max value
         byte mv = -99;//holds the current max
@@ -56,7 +46,7 @@ public class ItsChess {
         return v;
     }
     
-    public byte minValue(ItsChess.Board board, Integer a, Integer b)
+    public byte minValue(Board board, Integer a, Integer b)
     {
         byte v = 100;//initial min value
         byte mv = 99;//holds the current min
@@ -79,327 +69,20 @@ public class ItsChess {
         return v;
     }
     
-    public byte utility(ItsChess.Board board)
+    public byte utility(Board board)
     {
         //return 200(k) + 9(q) + 5(r) + 3(b+n) + (p) - 0.5(d+s+i) + 0.1(m);
         return 0;
     }
     
-    public byte[][] possibleMovesKnight(ItsChess.Board board, byte x, byte y)
-    {//returns a byte array of the possible moves a knight can make
-        byte[][] retval = new byte[8][2];
-        
-        if(board.theBoard[(x-1)][(y+2)] == 0)
-        {
-            retval[0][0] = (byte)(x-1);
-            retval[0][1] = (byte)(y+2);
-        }
-        if(board.theBoard[(x+1)][(y+2)] == 0)
-        {
-            retval[1][0] = (byte)(x+1);
-            retval[1][1] = (byte)(y+2);
-        }
-        if(board.theBoard[(x+2)][(y+1)] == 0)
-        {
-            retval[2][0] = (byte)(x+2);
-            retval[2][1] = (byte)(y+1);
-        }
-        if(board.theBoard[(x+2)][(y-1)] == 0)
-        {
-            retval[3][0] = (byte)(x+2);
-            retval[3][1] = (byte)(y-1);
-        }
-        if(board.theBoard[(x+1)][(y-2)] == 0)
-        {
-            retval[4][0] = (byte)(x+1);
-            retval[4][1] = (byte)(y-2);
-        }
-        if(board.theBoard[(x-1)][(y-2)] == 0)
-        {
-            retval[5][0] = (byte)(x-1);
-            retval[5][1] = (byte)(y-2);
-        }
-        if(board.theBoard[(x-2)][(y-1)] == 0)
-        {
-            retval[6][0] = (byte)(x-2);
-            retval[6][1] = (byte)(y-1);
-        }
-        if(board.theBoard[(x-2)][(y+1)] == 0)
-        {
-            retval[7][0] = (byte)(x-2);
-            retval[7][1] = (byte)(y+1);
-        }
-        
-        return retval;
-    }
-        
-    public byte[][] possibleMovesRook(ItsChess.Board board, byte x, byte y)
-    {//returns a byte array of the possible moves a rook can make
-        byte[][] retval = new byte[14][2];
-        byte sizecount = 0;
-        
-        for(byte i=x, m=y; i<8; i++)
-        {
-            if(board.theBoard[(i)][(m)] == 0)
-            {
-                retval[sizecount][0] = (byte)(i);
-                retval[sizecount][1] = (byte)(m);
-                sizecount++;
-            }
-            else
-            {
-                if(i != 7)
-                {
-                    retval[sizecount][0] = (byte)(i+1);
-                    retval[sizecount][1] = (byte)(m);
-                    sizecount++;
-                }
-                break;
-            }
-        }
-        for(byte i=x, m=y; i>0; i--)
-        {
-            if(board.theBoard[(i)][(m)] == 0)
-            {
-                retval[sizecount][0] = (byte)(i);
-                retval[sizecount][1] = (byte)(m);
-                sizecount++;
-            }
-            else
-            {
-                if(i != 1)
-                {
-                    retval[sizecount][0] = (byte)(i-1);
-                    retval[sizecount][1] = (byte)(m);
-                    sizecount++;
-                }
-                break;
-            }
-        }
-        for(byte i=x, m=y; m<8; m++)
-        {
-            if(board.theBoard[(i)][(m)] == 0)
-            {
-                retval[sizecount][0] = (byte)(i);
-                retval[sizecount][1] = (byte)(m);
-                sizecount++;
-            }
-            else
-            {
-                if(m != 7)
-                {
-                    retval[sizecount][0] = (byte)(i);
-                    retval[sizecount][1] = (byte)(m+1);
-                    sizecount++;
-                }
-                break;
-            }
-        }
-        for(byte i=x, m=y; m>0; m--)
-        {
-            if(board.theBoard[(i)][(m)] == 0)
-            {
-                retval[sizecount][0] = (byte)(i);
-                retval[sizecount][1] = (byte)(m);
-                sizecount++;
-            }
-            else
-            {
-                if(m != 1)
-                {
-                    retval[sizecount][0] = (byte)(i);
-                    retval[sizecount][1] = (byte)(m-1);
-                    sizecount++;
-                }
-                break;
-            }
-        }
-        
-        return retval;
-    }
-    
-    public byte[][] possibleMovesBishop(ItsChess.Board board, byte x, byte y)
-    {//returns a byte array of the possible moves a bishop can make
-        byte[][] retval = new byte[13][2];
-        byte sizecount = 0;
-        
-        for(byte i=x, m=y; i<8 && m<8; i++,m++)
-        {
-            if(board.theBoard[(i)][(m)] == 0)
-            {
-                retval[sizecount][0] = (byte)(i);
-                retval[sizecount][1] = (byte)(m);
-                sizecount++;
-            }
-            else
-            {
-                if(i != 7 && m != 7)
-                {
-                    retval[sizecount][0] = (byte)(i+1);
-                    retval[sizecount][1] = (byte)(m+1);
-                    sizecount++;
-                }
-                break;
-            }
-        }
-        for(byte i=x, m=y; i>0 && m<8; i--, m++)
-        {
-            if(board.theBoard[(i)][(m)] == 0)
-            {
-                retval[sizecount][0] = (byte)(i);
-                retval[sizecount][1] = (byte)(m);
-                sizecount++;
-            }
-            else
-            {
-                if(i != 1 && m != 7)
-                {
-                    retval[sizecount][0] = (byte)(i-1);
-                    retval[sizecount][1] = (byte)(m+1);
-                    sizecount++;
-                }
-                break;
-            }
-        }
-        for(byte i=x, m=y; i<8 && m>0; i++, m--)
-        {
-            if(board.theBoard[(i)][(m)] == 0)
-            {
-                retval[sizecount][0] = (byte)(i);
-                retval[sizecount][1] = (byte)(m);
-                sizecount++;
-            }
-            else
-            {
-                if(i != 7 && m != 1)
-                {
-                    retval[sizecount][0] = (byte)(i+1);
-                    retval[sizecount][1] = (byte)(m-1);
-                    sizecount++;
-                }
-                break;
-            }
-        }
-        for(byte i=x, m=y; i>0 && m>0; i--, m--)
-        {
-            if(board.theBoard[(i)][(m)] == 0)
-            {
-                retval[sizecount][0] = (byte)(i);
-                retval[sizecount][1] = (byte)(m);
-                sizecount++;
-            }
-            else
-            {
-                if(i != 1 && m != 1)
-                {
-                    retval[sizecount][0] = (byte)(i-1);
-                    retval[sizecount][1] = (byte)(m-1);
-                    sizecount++;
-                }
-                break;
-            }
-        }
-        
-        return retval;
-        
-        /*
-        
-        byte tx = x; byte ty = y;
-        
-        byte i=0;
-        for(; i<7; i++)
-        {
-            tx--; ty--;
-            if(tx < 1 || ty < 1)
-                break;
-        }
-        
-        tx = x; ty = y;
-        
-        byte j=0;
-        for(; j<7; j++)
-        {
-            tx++; ty++;
-            if(tx > 8 || ty > 8)
-                break;
-        }
-        
-        tx = x; ty = y;
-        
-        byte o=0;
-        for(; o<7; o++)
-        {
-            tx--; ty++;
-            if(tx < 1 || ty > 8)
-                break;
-        }
-        
-        tx = x; ty = y;
-        
-        byte p=0;
-        for(; p<7; p++)
-        {
-            tx++; ty--;
-            if(tx > 8 || ty < 1)
-                break;
-        }
-        
-        //------------------------------
-        
-        byte[][] retval = new byte[i+j+o+p][2];
-        
-        tx = x; ty = y;
-        for(int k=0; k<i; k++)
-        {
-            tx--; ty--;
-            if(board.theBoard[(tx)][(ty)] == 0)
-            {
-                retval[k][0] = (byte)(tx);
-                retval[k][1] = (byte)(ty);
-            }
-        }
-        
-        tx = x; ty = y;
-        for(int k=0; k<j; k++)
-        {
-            tx++; ty++;
-            if(board.theBoard[(tx)][(ty)] == 0)
-            {
-                retval[k+i][0] = (byte)(tx);
-                retval[k+i][1] = (byte)(ty);
-            }
-        }
-        
-        tx = x; ty = y;
-        for(int k=0; k<o; k++)
-        {
-            tx--; ty++;
-            if(board.theBoard[(tx)][(ty)] == 0)
-            {
-                retval[k+i+j][0] = (byte)(tx);
-                retval[k+i+j][1] = (byte)(ty);
-            }
-        }
-        
-        tx = x; ty = y;
-        for(int k=0; k<p; k++)
-        {
-            tx++; ty--;
-            if(board.theBoard[(tx)][(ty)] == 0)
-            {
-                retval[k+i+j+o][0] = (byte)(tx);
-                retval[k+i+j+o][1] = (byte)(ty);
-            }
-        }
-        
-        return retval; */
-    }
-    
-    
     //eval function chess //google search
     
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Board chessboard = new Board();
+        chessboard.analysis();
+        System.out.println(chessboard.toString());
+        
     }
 }
